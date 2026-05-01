9e8942c32b6dfc22ab45b7f32fa482cacb800a44
suah.nam <suah.nam@gmail.com>
2026-05-01 15:09:41 +0900

    add Textbook

---
+++ Textbook/src/ch02/sec02/LongExample.java
@@ -0,0 +1,14 @@
+package ch02.sec02;
+
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20;
+		long var3 = 10000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+
+}
