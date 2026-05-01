d175362a9018ecd700a343a404073001ab41ff22
suah.nam <suah.nam@gmail.com>
2026-05-01 15:07:55 +0900

    add CodingTest

---
+++ CodingTest/src/sub1/Solution.java
@@ -0,0 +1,14 @@
+package sub1;
+/**
+ * 날짜 : 2026/05/01
+ * 이름 : 남수아
+ * 내용 : 프로그래머스 PCCE 기출문제 1번 문자출력
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
