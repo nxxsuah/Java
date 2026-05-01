b325fc07dd5c9c2751c172b96099263a5a6a507c
suah.nam <suah.nam@gmail.com>
2026-05-01 14:45:30 +0900

    add Ch01 study

---
+++ Ch01/src/sub1/Hello.java
@@ -0,0 +1,22 @@
+package sub1;
+
+
+/*
+ * 날짜 : 2026/04/27
+ * 이름 : 남수아
+ * 내용 : 1장 Java 개발환경 구축 실습
+ */
+//Crtl + : 확대, Ctrl - : 축소
+public class Hello {
+	
+	
+	public static void main(String[] args) {
+		
+		// 모든 프로그래밍 언어의 상징적인 입문 문자열
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+		
+		
+	}
+
+}
