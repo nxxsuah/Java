1cbe10ebb30bacc53a31e2a97a0c6779a7f07af0
suah.nam <suah.nam@gmail.com>
2026-05-04 12:47:49 +0900

    add Textbook

---
+++ Textbook/src/ch04/sec8/CountinueExample.java
@@ -0,0 +1,13 @@
+package ch04.sec8;
+
+public class CountinueExample {
+	public static void main(String[] args) throws Exception {
+		for(int i = 1; i <=10; i++) {
+			if (i%2 !=0) {
+				continue;
+				}
+			System.out.println(i + " ");
+		}
+	}
+
+}
